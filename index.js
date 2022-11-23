03b5bbcc455c31fac5286c24ab24ccfed85b952d
prince475 <danielmwai475@gmail.com>
2022-11-23 07:52:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+
+document.createElement('tagName')
